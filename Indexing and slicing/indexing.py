5077ab42b9ad58518f09ac90ab243d3d583d7f49
sakshipatel09765-cmyk <sakshipatel09765@gmail.com>
2026-07-21 21:29:06 +0530

    Add Numpy indexing and slicing topics

---
+++ Indexing and slicing/indexing.py
@@ -0,0 +1,11 @@
+#indexing start from 0
+#negative indexing starts from -1
+#array[index] -> 1D array
+#array[row, coloumn] -> 2D array
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+mydata = arr[0]
+print(mydata)
+print(arr[-1])
---
+++ Indexing and slicing/slicing.py
@@ -0,0 +1,7 @@
+#extact a subarray from an main array
+#arr[start; stop ; step]
+#arr[1; 10 ; 1] -> 10 exclude rehta h means stop pe jo element likhoge woh exclude rahega
+
+import numpy as np
+
+myarr = np.array([1,2,3,4,5,6,7,8,9,10])
